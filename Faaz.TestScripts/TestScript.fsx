448fddca1f0d39f8830d72497d1c0c3752030229
Max Clifford <me@mclifford.dev>
2024-10-12 14:55:40 +0100

    Added test script

---
+++ Faaz.TestScripts/TestScript.fsx
@@ -0,0 +1,22 @@
+﻿#r "../Faaz/bin/Debug/net6.0/Faaz.dll"
+#r "../Faaz/bin/Debug/net6.0/Fipc.Core.dll"
+//#r ""
+
+open Faaz.ScriptingContext
+open Fipc.Core.Common
+
+let test pipeName =
+    try
+        //let pipeName = "faaz-test"
+        use pipe = connect pipeName
+        pipe.TryPostMessage(FipcMessage.StringMessage("Hello, World!")) |> ignore
+        pipe.TryPostMessage(FipcMessage.StringMessage("This is a message from a script!")) |> ignore
+        pipe.TryPostMessage(FipcMessage.StringMessage($"The pipe name is `{pipeName}`")) |> ignore
+        pipe.TryPostMessage(FipcMessage.StringMessage("Waiting a second")) |> ignore
+        Async.Sleep 1000 |> Async.RunSynchronously
+        pipe.TryPostMessage(FipcMessage.StringMessage("Good night!")) |> ignore
+        0
+    with
+    | exn ->
+        printfn $"Error {exn.Message}"
+        -1
