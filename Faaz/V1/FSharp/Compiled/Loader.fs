b25d6b23d9d34412c865048380520907b0d9be05
Max <me@mclifford.dev>
2025-05-16 22:38:10 +0100

    feat(Fsharp.Compiled): Added Loader.fs
    Added Loader.fs

---
+++ Faaz/V1/FSharp/Compiled/Loader.fs
@@ -0,0 +1,12 @@
+﻿namespace Faaz.V1.FSharp.Compiled
+
+open System.Reflection
+
+module Loader =
+    
+    let loadAssembly (path: string) =
+        try
+            Assembly.LoadFile path |> Ok
+        with
+        | ex -> Error ex.Message
+
