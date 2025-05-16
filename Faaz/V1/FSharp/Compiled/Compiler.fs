5ba0a740358d14c6c879e0f1a29df9d04377eab4
Max <me@mclifford.dev>
2025-05-16 22:39:35 +0100

    feat(FSharp.Compiled): Work on FSharp.Compiled
    Work on FSharp.Compiled

---
+++ Faaz/V1/FSharp/Compiled/Compiler.fs
@@ -0,0 +1,179 @@
+﻿namespace Faaz.V1.FSharp.Compiled
+
+open System
+open System.IO
+open System.Security.Cryptography
+open System.Text.RegularExpressions
+open FSharp.Compiler.CodeAnalysis
+open FSharp.Compiler.Diagnostics
+
+module Compiler =
+
+    type CompilerConfiguration =
+        {
+
+            //AllSigns: bool
+
+
+            /// Compiler option: -a [file name]
+            OutputPath: string
+            TargetProfile: string
+            Options: CompilerOptions
+        }
+
+    and CompilerOptions =
+        {
+            /// Compiler option: --allsigs
+            AllSign: bool
+        }
+
+        static member Default = { AllSign = false }
+
+    type CompilerContext =
+        private
+            { Checker: FSharpChecker
+              Configuration: CompilerConfiguration }
+
+        static member Create(cfg: CompilerConfiguration) =
+            { Checker = FSharpChecker.Create()
+              Configuration = cfg }
+
+    type CompilerError =
+        | InvalidFileType of Extension: string
+        | CompilerFailure of Errors: FSharpDiagnostic array * Exception: exn option
+
+    [<RequireQualifiedAccess>]
+    type ScriptDependencyType =
+        | Script of string
+        | Dll of string
+        | Nuget of string
+
+    type HashType =
+        | MD5
+        | SHA256
+        
+    [<AutoOpen>]
+    module private Internal =
+
+        let loadRegex =
+            Regex("#load[ \t]*\"(?<path>(.*?))\"", RegexOptions.Singleline ||| RegexOptions.Compiled)
+
+        let referenceRegex =
+            Regex("#r[ \t]*\"(?<path>(.*?))\"", RegexOptions.Singleline ||| RegexOptions.Compiled)
+
+        let nugetRegex =
+            Regex("#r[ \t]*\"nuget:[ \t]*(?<path>(.*?))\"", RegexOptions.Singleline ||| RegexOptions.Compiled)
+
+        let makeArgs (ctx: CompilerContext) (scriptPath: string) (outputAssemblyName: string) =
+            [| "fsc.exe"
+               "-o"
+               outputAssemblyName
+               "-a"
+               scriptPath
+               $"--targetprofile:{ctx.Configuration.TargetProfile}" |]
+
+        let makeDllName (scriptName: string) (hash: string) = $"{scriptName}__{hash}.dll"
+
+        let getDependencies (scriptPath: string) =
+            File.ReadLines(scriptPath)
+            |> Seq.choose (fun line ->
+                //match line with
+                try
+                    match line with
+                    | _ when nugetRegex.IsMatch(line) ->
+                        //let matches =
+                        nugetRegex.Matches(line).[0].Groups["path"].Value
+                        |> ScriptDependencyType.Nuget
+                        |> Some
+                    | _ when referenceRegex.IsMatch(line) ->
+                        referenceRegex.Matches(line).[0].Groups["path"].Value
+                        |> ScriptDependencyType.Dll
+                        |> Some
+                    | _ when loadRegex.IsMatch(line) ->
+                        loadRegex.Matches(line).[0].Groups["path"].Value
+                        |> ScriptDependencyType.Script
+                        |> Some
+                    | _ -> None
+                with _ ->
+                    // This is a best-effect attempted to find dependencies.
+                    // In the case of an error being thrown simply skip.
+                    // This will likely cause a re-compilation,
+                    // but that is not an issue.
+                    None)
+            
+        let hashFile (hasher: HashAlgorithm) (path: string) =
+            use fs = File.OpenRead path
+            
+            hasher.ComputeHash(fs)
+            |> fun hashBytes -> BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant()
+            
+            //hasher.ComputeHashAsync
+            //
+            //use crypto = new CryptoStream(fs, hasher, CryptoStreamMode.Read)
+            //
+            ////do! File.ReadAllBytesAsync(path) |> hasher.ComputeHashAsync
+            //
+            //File.ReadAllBytes(path)  
+        
+        let p (path: string) = Path.IsPathFullyQualified(path)
+        
+        //let p (path: string) = Path.getf
+            
+            
+        let generateHash (scriptPath: string) =
+            
+            let hasher = SHA256.Create()
+            
+            
+            let rec traverse (path: string) =
+                ()
+            
+            
+            ()
+
+    // NOT supported in the lastest version of FSC
+    //let compileScriptToDynamicAssemblyAsync (cfg: CompilerConfiguration) (path: string) =
+    //async {
+    //    let outputAssemblyName = makeDllName (Path.GetFileName(path))
+    //
+    //    let checker = FSharpChecker.Create()
+    //
+    //    let! errors, exitCode, dynamicAssemblyOption =
+    //        checker.CompileToDynamicAssembly(
+    //            [| "fsc.exe"
+    //               "-o"
+    //               outputAssemblyName
+    //               "-a"
+    //               path
+    //               $"--targetprofile:{cfg.TargetProfile}" |],
+    //            Some(stdout, stderr)
+    //        )
+    //
+    //    match exitCode, dynamicAssemblyOption with
+    //    | 0, Some dynamicAssembly -> return Ok dynamicAssembly
+    //    | _ -> return errors |> Error
+    //}
+    //
+    let compileScriptAsync (ctx: CompilerContext) (path: string) =
+        async {
+
+            match Path.GetExtension(path) with
+            | ".fsx" ->
+                let scriptName = Path.GetFileNameWithoutExtension(path)
+
+                // TODO check output path for files with same script name.
+                // If found then check the hash with this scripts hash.
+                // If same, do nothing.
+
+                let dllName = makeDllName scriptName "TODO"
+
+                let outputAssemblyName = Path.Combine(ctx.Configuration.OutputPath, dllName)
+
+                match! ctx.Checker.Compile(makeArgs ctx path outputAssemblyName) with
+                | (_, None) -> return Ok outputAssemblyName
+                | (errors, exnOpt) -> return Error(CompilerError.CompilerFailure(errors, exnOpt))
+            | ext -> return Error(CompilerError.InvalidFileType ext)
+        }
+
+    let compileScript (ctx: CompilerContext) (scriptPath: string) =
+        compileScriptAsync ctx scriptPath |> Async.RunSynchronously
---
+++ Faaz/V1/FSharp/Compiled/Extractor.fs
@@ -0,0 +1,99 @@
+﻿namespace Faaz.V1.FSharp.Compiled
+
+open System
+open System.Linq.Expressions
+open System.Reflection
+
+module Extractor =
+
+    module Internal =
+
+        let defaultBindingFlags = BindingFlags.Static ||| BindingFlags.Public
+
+        let getMemberInfo (name: string) (assembly: Assembly) (bindingFlags: BindingFlags) =
+            let fqTypeName, memberName =
+                let splitIndex = name.LastIndexOf(".")
+
+                name.[0 .. splitIndex - 1], name.[splitIndex + 1 ..]
+
+            let candidates =
+                assembly.GetTypes()
+                |> Seq.where (fun t -> t.FullName = fqTypeName)
+                |> Seq.toList
+
+            match candidates with
+            | [ t ] ->
+                match t.GetMethod(memberName, bindingFlags) with
+                | null -> Error ""
+                | memberInfo -> Ok memberInfo
+            | [] -> Error ""
+            | _ -> Error ""
+
+        let extractor<'r> name assembly parameters =
+            match getMemberInfo name assembly defaultBindingFlags with
+            | Ok memberInfo ->
+                let lambda =
+                    let expression =
+                        if (typeof<'r> = typeof<unit>) then
+                            Expression.Block(
+                                Expression.Call(memberInfo, parameters |> Array.map (fun param -> param :> Expression)),
+                                Expression.Constant((), typeof<'r>)
+                            )
+                            :> Expression
+                        else
+                            Expression.Convert(
+                                Expression.Call(memberInfo, parameters |> Array.map (fun param -> param :> Expression)),
+                                typeof<'r>
+                            )
+                            :> Expression
+
+                    Expression.Lambda (expression, parameters)
+
+                let systemFunc = lambda.Compile()
+
+                systemFunc |> Ok
+
+            | Error e -> Error ""
+
+        let extractFunction1<'T1, 'TResult> name (assembly: Assembly) : Result<'T1 -> 'TResult, string> =
+            [| Expression.Parameter(typeof<'T1>) |]
+            |> extractor<'TResult> name assembly
+            |> Result.map (fun systemFunc -> systemFunc :?> Func<'T1, 'TResult> |> FuncConvert.FromFunc)
+
+        let extractFunction2<'T1, 'T2, 'TResult> name (assembly: Assembly) : Result<'T1 -> 'T2 -> 'TResult, string> =
+            [| Expression.Parameter(typeof<'T1>); Expression.Parameter(typeof<'T2>) |]
+            |> extractor<'TResult> name assembly
+            |> Result.map (fun systemFunc -> systemFunc :?> Func<'T1, 'T2, 'TResult> |> FuncConvert.FromFunc)
+
+        let extractFunction3<'T1, 'T2, 'T3, 'TResult> name (assembly: Assembly) =
+            [| Expression.Parameter(typeof<'T1>)
+               Expression.Parameter(typeof<'T2>)
+               Expression.Parameter(typeof<'T3>) |]
+            |> extractor<'TResult> name assembly
+            |> Result.map (fun systemFunc -> systemFunc :?> Func<'T1, 'T2, 'T3, 'TResult> |> FuncConvert.FromFunc)
+
+        let extractFunction4<'T1, 'T2, 'T3, 'T4, 'TResult> name (assembly: Assembly) =
+            [| Expression.Parameter(typeof<'T1>)
+               Expression.Parameter(typeof<'T2>)
+               Expression.Parameter(typeof<'T3>)
+               Expression.Parameter(typeof<'T4>) |]
+            |> extractor<'TResult> name assembly
+            |> Result.map (fun systemFunc -> systemFunc :?> Func<'T1, 'T2, 'T3, 'T4, 'TResult> |> FuncConvert.FromFunc)
+
+
+    type FunctionExtractor(assembly: Assembly) =
+
+        member _.TryGetFunction<'T1, 'TResult>(name) =
+            Internal.extractFunction1<'T1, 'TResult> name assembly
+
+        member _.TryGetFunction<'T1, 'T2, 'TResult>(name) =
+            Internal.extractFunction2<'T1, 'T2, 'TResult> name assembly
+
+        member _.TryGetFunction<'T1, 'T2,'T3, 'TResult>(name) =
+            Internal.extractFunction3<'T1, 'T2,'T3, 'TResult> name assembly
+
+        member _.TryGetFunction<'T1, 'T2, 'T3, 'T4, 'TResult>(name) =
+            Internal.extractFunction4<'T1, 'T2, 'T3, 'T4, 'TResult> name assembly
+
+       
+        
