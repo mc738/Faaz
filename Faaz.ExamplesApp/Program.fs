9e731b25bd66cf2242987d503edc0538907f41e1
Max <me@mclifford.dev>
2025-05-17 23:54:44 +0100

    feat(Faaz.ExampleApp): Added logging to Program.fs
    Added logging ot the program

--- Faaz.ExamplesApp/Program.fs
+++ Faaz.ExamplesApp/Program.fs
@@ -1,46 +1,54 @@
-﻿
+﻿open Microsoft.Extensions.Logging
 
 module FSharpCompiled =
-    
-    
-    
+
+
+
     module HelloWorld =
-        
+
         open Faaz.V1.FSharp.Compiled
-        
-        let run () =
-            let scriptPath = "C:\\Users\\mclif\\Projects\\dotnet\\Faaz\\Faaz.ExamplesApp\\Scripts\\FSharp\\HelloWorld.fsx"
-            
-            let cfg = ({
-                OutputPath = "C:\\Users\\mclif\\Projects\\data\\Faaz\\dlls"
-                TargetProfile = "netcore"
-                Options = Compiler.CompilerOptions.Default
-            } : Compiler.CompilerConfiguration)
-            
-            let ctx = Compiler.CompilerContext.Create(cfg)
-            
+
+        let run (logger: ILogger) =
+            let scriptPath =
+                "C:\\Users\\mclif\\Projects\\dotnet\\Faaz\\Faaz.ExamplesApp\\Scripts\\FSharp\\ComplexScript.fsx"
+
+            let cfg =
+                ({ OutputPath = "C:\\Users\\mclif\\Projects\\data\\Faaz\\dlls"
+                   TargetProfile = "netcore"
+                   RecompileOption = Compiler.RecompileOption.OnHashChanged
+                   HashConfiguration = Compiler.HashConfiguration.Default
+                   Options = Compiler.CompilerOptions.Default }
+                : Compiler.CompilerConfiguration)
+
+            let ctx = Compiler.CompilerContext.Create(cfg, logger)
+
             match Compiler.compileScript ctx scriptPath with
             | Ok resultValue ->
                 match Loader.loadAssembly resultValue with
                 | Ok assembly ->
                     let extractor = Extractor.FunctionExtractor(assembly)
-                    
-                    match extractor.TryGetFunction<string option, unit>("HelloWorld.run") with
+
+                    match extractor.TryGetFunction<string option, unit>("ComplexScript.run") with
                     | Ok resultValue ->
                         resultValue (Some "Max")
                         resultValue None
-                        //resultValue "Max"
+                    //resultValue "Max"
                     | Error errorValue -> failwith "todo"
                 | Error errorValue -> failwith "todo"
             | Error errorValue -> failwith "todo"
-            
-            
+
+
             ()
-        
-        
-    
-    
-    
+
+
+
+
+
     ()
 
-FSharpCompiled.HelloWorld.run ()
+let loggerFactory =
+    LoggerFactory.Create(fun builder -> builder.AddConsole().AddFilter("Faaz.ExamplesApp", LogLevel.Trace) |> ignore)
+
+let logger = loggerFactory.CreateLogger("Faaz.ExamplesApp")
+logger.LogInformation("Starting examples app")
+FSharpCompiled.HelloWorld.run logger
