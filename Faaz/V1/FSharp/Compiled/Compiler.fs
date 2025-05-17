5c31301b707dcf06933b6c43df7c8b3ffd52c2a9
Max <me@mclifford.dev>
2025-05-17 23:44:50 +0100

    feat(FSharpCompiled): Clean up
    Clean up of Compiler.fs

--- Faaz/V1/FSharp/Compiled/Compiler.fs
+++ Faaz/V1/FSharp/Compiled/Compiler.fs
@@ -1,29 +1,69 @@
 ﻿namespace Faaz.V1.FSharp.Compiled
 
 open System
+open System.Globalization
 open System.IO
 open System.Security.Cryptography
+open System.Text
 open System.Text.RegularExpressions
 open FSharp.Compiler.CodeAnalysis
 open FSharp.Compiler.Diagnostics
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Logging.Abstractions
+open FsToolbox.Extensions.Strings
 
 module Compiler =
 
+    [<RequireQualifiedAccess>]
+    type RecompileOption =
+        | Always
+        | OnHashChanged
+
+
+    [<RequireQualifiedAccess>]
+    type HashMode =
+        | ScriptOnly
+        | ScriptAndDependencies
+
+    type HashConfiguration =
+        { Type: HashType
+          Mode: HashMode }
+
+        static member Default =
+            { Type = HashType.SHA256
+              Mode = HashMode.ScriptAndDependencies }
+
+    and [<RequireQualifiedAccess>] ChangeDetectionMode =
+        | Hash of HashType
+
+        override cdt.ToString() =
+            match cdt with
+            | Hash hashType -> $"Hash (hash type: {hashType})"
+
+    and [<RequireQualifiedAccess>] HashType =
+        | MD5
+        | SHA256
+
+        override ht.ToString() =
+            match ht with
+            | MD5 -> "MD5"
+            | SHA256 -> "SHA256"
+
     type CompilerConfiguration =
         {
-
-            //AllSigns: bool
-
-
-            /// Compiler option: -a [file name]
             OutputPath: string
+            /// <summary>
+            /// --targetprofile:[profile]
+            /// </summary>
             TargetProfile: string
+            RecompileOption: RecompileOption
+            HashConfiguration: HashConfiguration
             Options: CompilerOptions
         }
 
     and CompilerOptions =
         {
-            /// Compiler option: --allsigs
+            /// <summary>Compiler option: --allsigs</summary>
             AllSign: bool
         }
 
@@ -32,10 +72,12 @@ module Compiler =
     type CompilerContext =
         private
             { Checker: FSharpChecker
+              Logger: ILogger
               Configuration: CompilerConfiguration }
 
-        static member Create(cfg: CompilerConfiguration) =
+        static member Create(cfg: CompilerConfiguration, ?logger: ILogger) =
             { Checker = FSharpChecker.Create()
+              Logger = logger |> Option.defaultWith (fun _ -> NullLogger.Instance)
               Configuration = cfg }
 
     type CompilerError =
@@ -48,12 +90,19 @@ module Compiler =
         | Dll of string
         | Nuget of string
 
-    type HashType =
-        | MD5
-        | SHA256
+    [<RequireQualifiedAccess>]
+    type ChangeDetectionResult =
+        | NoChange of PreviousDllPath: string
+        | Skipped of NewHash: string
+        | HashChanged of NewHash: string
 
     [<AutoOpen>]
     module private Internal =
+
+        let timestampFormat = "yyyyMMddHHmmss"
+
+        let fileNameHashRegex =
+            Regex("^(.*?)___(?<hash>(.*?))_(?<timestamp>(.*?)).dll$", RegexOptions.Singleline ||| RegexOptions.Compiled)
 
         let loadRegex =
             Regex("#load[ \t]*\"(?<path>(.*?))\"", RegexOptions.Singleline ||| RegexOptions.Compiled)
@@ -72,7 +121,8 @@ module Compiler =
                scriptPath
                $"--targetprofile:{ctx.Configuration.TargetProfile}" |]
 
-        let makeDllName (scriptName: string) (hash: string) = $"{scriptName}__{hash}.dll"
+        let makeDllName (scriptName: string) (hash: string) =
+            $"{scriptName}___{hash}_{DateTime.UtcNow:timestampFormat}.dll"
 
         let getDependencies (scriptPath: string) =
             File.ReadLines(scriptPath)
@@ -83,16 +133,16 @@ module Compiler =
                     | _ when nugetRegex.IsMatch(line) ->
                         //let matches =
                         nugetRegex.Matches(line).[0].Groups["path"].Value
-                        |> ScriptDependencyType.Nuget
-                        |> Some
+                        |> _.ToOption()
+                        |> Option.map ScriptDependencyType.Nuget
                     | _ when referenceRegex.IsMatch(line) ->
                         referenceRegex.Matches(line).[0].Groups["path"].Value
-                        |> ScriptDependencyType.Dll
-                        |> Some
+                        |> _.ToOption()
+                        |> Option.map ScriptDependencyType.Dll
                     | _ when loadRegex.IsMatch(line) ->
                         loadRegex.Matches(line).[0].Groups["path"].Value
-                        |> ScriptDependencyType.Script
-                        |> Some
+                        |> _.ToOption()
+                        |> Option.map ScriptDependencyType.Script
                     | _ -> None
                 with _ ->
                     // This is a best-effect attempted to find dependencies.
@@ -105,7 +155,11 @@ module Compiler =
             use fs = File.OpenRead path
 
             hasher.ComputeHash(fs)
-            |> fun hashBytes -> BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant()
+        //|> fun hashBytes -> BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant()
+
+        let bytesToString (bytes: byte array) =
+            BitConverter.ToString(bytes).Replace("-", "").ToLowerInvariant()
+
 
         //hasher.ComputeHashAsync
         //
@@ -119,11 +173,16 @@ module Compiler =
 
         //let p (path: string) = Path.getf
 
-        let generateHash (scriptPath: string) =
+        let createHasher (hashType: HashType) =
+            match hashType with
+            | HashType.MD5 -> MD5.Create() :> HashAlgorithm
+            | HashType.SHA256 -> SHA256.Create() :> HashAlgorithm
 
-            let hasher = SHA256.Create()
+        let generateHash (ctx: CompilerContext) (hasher: HashAlgorithm) (scriptPath: string) =
+            ctx.Logger.LogTrace("Generating hash for script {ScriptPath} and dependencies.", scriptPath)
 
             let rec traverse (path: string) =
+
                 let fileDirectory = Path.GetDirectoryName(path)
 
                 let getDependencyPath (path: string) = Path.GetFullPath(path, fileDirectory)
@@ -138,16 +197,74 @@ module Compiler =
                         | ScriptDependencyType.Dll dllPath -> hashFile hasher dllPath
                         | ScriptDependencyType.Nuget nugetPath ->
                             // TODO nuget not supported yet - for now just generate a random guid
-                            Guid.NewGuid().ToString("n"))
+                            Guid.NewGuid().ToByteArray())
+
+                ctx.Logger.LogTrace("Hashing file {Path} and combining with any dependents", path)
 
                 seq {
                     fileHash
                     yield! dependencyHashes
                 }
-                |> String.concat ""
+                |> Array.concat
 
-            traverse scriptPath
+            traverse scriptPath |> hasher.ComputeHash |> bytesToString
 
+        let ``get previous hash and timestamp from file name`` (fileInfo: FileInfo) =
+            let m = fileNameHashRegex.Match(fileInfo.Name)
+
+            if m.Success then
+                {| FilePath = fileInfo.FullName
+                   Hash = m.Groups["hash"].Value
+                   Timestamp =
+                    match
+                        DateTime.TryParseExact(
+                            m.Groups["timestamp"].Value,
+                            timestampFormat,
+                            CultureInfo.InvariantCulture,
+                            DateTimeStyles.AssumeUniversal
+                        )
+                    with
+                    | true, v -> v
+                    | false, _ ->
+                        // If the previous timestamp can not be exacted from the file now,
+                        // use the file creation time.
+                        // This is less stable but will do for this cirumstance.
+                        fileInfo.CreationTimeUtc |}
+                |> Some
+            else
+                None
+
+        let ``get previous dll hash`` (directoryPath: string) (scriptName: string) =
+            DirectoryInfo(directoryPath).EnumerateFiles($"{Path.GetFileNameWithoutExtension(scriptName)}___*.dll")
+            |> Seq.choose ``get previous hash and timestamp from file name``
+            |> Seq.sortByDescending (fun dll -> dll.Timestamp)
+            |> Seq.tryHead
+            |> Option.map (fun r -> r.Hash, r.FilePath)
+
+        let ``check for changes`` (ctx: CompilerContext) (scriptPath: string) (forceRecompile: bool) =
+            let hasher = createHasher ctx.Configuration.HashConfiguration.Type
+
+            let newHash =
+                match ctx.Configuration.HashConfiguration.Mode with
+                | HashMode.ScriptOnly -> hashFile hasher scriptPath |> bytesToString
+                | HashMode.ScriptAndDependencies -> generateHash ctx hasher scriptPath
+
+            match ctx.Configuration.RecompileOption, forceRecompile with
+            | RecompileOption.Always, _
+            | _, true ->
+                ctx.Logger.LogTrace("Recompile option set to {RecompileOption}, skipping checks.", "Always")
+
+                ChangeDetectionResult.Skipped newHash
+            | RecompileOption.OnHashChanged, false ->
+                ctx.Logger.LogTrace("Recompile option set to {RecompileOption}, skipping checks.", "OnHashChanged")
+
+                match ``get previous dll hash`` ctx.Configuration.OutputPath scriptPath with
+                | None -> ChangeDetectionResult.HashChanged newHash
+                | Some(previousHash, filePath) ->
+                    if newHash.Equals(previousHash, StringComparison.OrdinalIgnoreCase) |> not then
+                        ChangeDetectionResult.HashChanged newHash
+                    else
+                        ChangeDetectionResult.NoChange filePath
     // NOT supported in the lastest version of FSC
     //let compileScriptToDynamicAssemblyAsync (cfg: CompilerConfiguration) (path: string) =
     //async {
@@ -171,24 +288,33 @@ module Compiler =
     //    | _ -> return errors |> Error
     //}
     //
-    let compileScriptAsync (ctx: CompilerContext) (path: string) =
+    let compileScriptAsync (ctx: CompilerContext) (scriptPath: string) =
         async {
 
-            match Path.GetExtension(path) with
+            match Path.GetExtension(scriptPath) with
             | ".fsx" ->
-                let scriptName = Path.GetFileNameWithoutExtension(path)
+                let scriptName = Path.GetFileNameWithoutExtension(scriptPath)
 
                 // TODO check output path for files with same script name.
                 // If found then check the hash with this scripts hash.
                 // If same, do nothing.
 
-                let dllName = makeDllName scriptName "TODO"
+                match ``check for changes`` ctx scriptPath false with
+                | ChangeDetectionResult.NoChange previousDllPath ->
+                    ctx.Logger.LogTrace("No changes detected to script {ScriptPath}. Skipping compilation.", scriptPath)
+                    return Ok previousDllPath
+                | ChangeDetectionResult.Skipped newHash
+                | ChangeDetectionResult.HashChanged newHash ->
+                    ctx.Logger.LogTrace("Changes detected to script {ScriptPath}. Compiling script.", scriptPath)
 
-                let outputAssemblyName = Path.Combine(ctx.Configuration.OutputPath, dllName)
 
-                match! ctx.Checker.Compile(makeArgs ctx path outputAssemblyName) with
-                | (_, None) -> return Ok outputAssemblyName
-                | (errors, exnOpt) -> return Error(CompilerError.CompilerFailure(errors, exnOpt))
+                    let dllName = makeDllName scriptName newHash
+
+                    let outputAssemblyName = Path.Combine(ctx.Configuration.OutputPath, dllName)
+
+                    match! ctx.Checker.Compile(makeArgs ctx scriptPath outputAssemblyName) with
+                    | (_, None) -> return Ok outputAssemblyName
+                    | (errors, exnOpt) -> return Error(CompilerError.CompilerFailure(errors, exnOpt))
             | ext -> return Error(CompilerError.InvalidFileType ext)
         }
 
